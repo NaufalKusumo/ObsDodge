4f15e9d62cbea84d6362eb95e097867f2a9360c0
Naufal N. Kusumo <nopalji375@gmail.com>
2025-03-30 16:01:34 +0700

    Move

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,7 +8,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // [SerializeField] float xValue = 0.05f;
     // [SerializeField] float yValue = 0.5f;
     // float zValue = 0.05f;
-    [SerializeField] float speed = 0.05f;
+    [SerializeField] float MoveSpeed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +19,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float yValue = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(xValue, yValue, 0f); 
+        float xValue = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+        transform.Translate(xValue, 0f, zValue); 
         // transform.Rotate(50, 0, 0);
         
     }
