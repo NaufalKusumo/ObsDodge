5da4bab897966524d954bbe8280e8c0f6dbe0c0f
Naufal N. Kusumo <nopalji375@gmail.com>
2025-03-31 16:54:03 +0700

    Update ObjectHit.cs

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other) 
     {
-        Debug.Log("Hit! by " + other);    
+        GetComponent<MeshRenderer>().material.color = Color.red;  
+        Debug.Log("Hit! by " + other);  
     }
 }
