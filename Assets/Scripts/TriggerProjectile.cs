34e2d108e6069f71ee7d73f95ff70eb027a38a5a
Naufal N. Kusumo <nopalji375@gmail.com>
2025-04-02 19:32:31 +0700

    trigger

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 public class TriggerProjectile : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameObject[] projectiles;
+
+    void Start()
+    {
+        projectiles = GameObject.FindGameObjectsWithTag("Projectile");
+        Debug.Log($"Found {projectiles.Length} projectiles");
+
+    }
     void OnTriggerEnter(Collider other)
     {
-        
+        foreach(GameObject projectile in projectiles)
+        {
+            projectile.SetActive(true);
+        }
     }
 }
